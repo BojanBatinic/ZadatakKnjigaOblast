ab17a6338a3dc0f43f9c4f3d61b5057af2cc9cf0
Bojan <new@mail.com>
2017-10-05 21:13:08 +0200

    Uradjen zadatak izmena vrednosti fixed #9

--- ZadatakKnjigaOblast/src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ ZadatakKnjigaOblast/src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,11 +1,19 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
+
+    static Dao<Knjiga,Integer> knjigaDao;
+    static Dao<Oblast,Integer> oblastDao;
 
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
@@ -14,6 +22,24 @@ public class Zadatak3IzmenaVrednosti {
                naredbe bazi
             */
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+
+            knjigaDao = DaoManager.createDao(connectionSource, Knjiga.class);
+            oblastDao = DaoManager.createDao(connectionSource, Oblast.class);
+
+            List<Oblast> oblasti = oblastDao.queryForAll();
+            System.out.println("Prikaz svih oblasti da proverimo da li je uspisana oblast ciju vrednost menjamo.");
+            for(Oblast o : oblasti)
+                System.out.println(o);
+
+            Oblast izmena = oblastDao.queryForEq(Oblast.POLJE_NAZIV, "Activity klasa").get(0);
+
+            izmena.setPocetnaStrana(35);
+            oblastDao.update(izmena);
+
+            oblasti = oblastDao.queryForAll();
+            System.out.println("\nPrikaz svih oblasti da proverimo da li je uspisana promenjena vrednost.");
+            for(Oblast o : oblasti)
+                System.out.println(o);
 
         }catch (Exception e) {
             e.printStackTrace();
